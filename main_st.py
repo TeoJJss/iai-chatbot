85bcff56b499cfccc5833ed48a4b6f1133be7484
teojunjia <teojunjia@gmail.com>
2024-01-27 18:52:10 +0800

    streamlit support

---
+++ main_st.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from responses import reply
+import asyncio
+from PIL import Image
+
+st.set_page_config(page_title="APU Live Chatbot")
+    
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Hi, I am APU Virtual Bot. You may ask me anything about the facilities and services in APU."}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"], unsafe_allow_html=True)
+
+if user_input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.write(user_input)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = str(asyncio.run(reply(user_input))).strip().replace("\n", "<br>")
+            st.markdown(response, unsafe_allow_html=True) 
+            if "hi" == user_input:
+                bot_image = Image.open("images/hi.gif")
+                st.image(bot_image, caption="hi", use_column_width=True)
+            if "parking rate" in response.lower():
+                bot_image = Image.open("images/apu_map.jpeg")
+                st.image(bot_image, caption="APU Map", use_column_width=True)
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
